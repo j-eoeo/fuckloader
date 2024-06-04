e624d27530529df17641bc0ab4a622f9b33242aa
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-06-04 22:15:23 +0900

    a

--- commands/utility/sugarcoat.js
+++ commands/utility/sugarcoat.js
@@ -25,13 +25,26 @@ module.exports = {
         console.log("get moie title");
         var titlerg = /[!"#$%&'\(\)\^\\=~\|@\`\[\]\{\};:\+\*,.<>/\?\_\s]/g;
         var title   = info.player_response.videoDetails.title.replace(titlerg, "_");
-        var option  = {
-            quality: "highest",
+        var video_option  = {
+            quality: "highestvideo",
             filter: format => format.container === "mp4"
         };
-        console.log("starting donwlaod");
 
-        ytdl(url, option).pipe(fs.createWriteStream(`${distdir}/${title}.mp4`));
+        var audio_option  = {
+            quality: "highestaudio"
+        };
+
+        console.log("starting video donwlaod");
+        ytdl(url, video_option).pipe(fs.createWriteStream(`${distdir}/${title}_video.mp4`));
+
+        console.log("starting audio downlaod");
+        ytdl(url, audio_option).pipe(fs.createWriteStream(`${distdir}/${title}_audio.wav`));
+
+        console.log("comninbe things");
+        exec(`ffmpeg -y -i ${distdir}/${title}_video.mp4 -i ${distdir}/${title}_audio.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 ${distdir}/${title}.mp4`), (err, stdout, stderr) => {
+            interaction.editReply("https://assault1892.boats/sugarcoat/Theyre_Not_Gonna_Sugarcoat_It.png");
+        };
+
         console.log("downloa dcomplete\n")
 
         interaction.editReply(`damn\nhttps://assault1892.boats/sugarcoat/${title}.mp4`)
